ecefa66707b424614571df1344f715f9eb7a0d9c
jiulen <wongjiulen.coding@gmail.com>
2024-01-28 04:25:44 +0800

    credits setup

---
+++ Assets/Script/CreditsPanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsPanel : MonoBehaviour
+{
+    public void ShowCredits(bool show)
+    {
+        gameObject.SetActive(show);
+    }
+}
